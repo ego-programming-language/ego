4e70f59291d5d7027ca3d7433ca3949f5a9b634d
noreplydev <contact.cristiansanchez@gmail.com>
2024-10-30 23:11:29 +0100

    Add cleaner way to instantiate self instance

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,7 +1,5 @@
-use self_vm::vm::Vm;
-
 fn main() {
-    let bytecode = ego::gen_bytecode("print(12)".to_string());
-    let mut vm = Vm::new(bytecode);
+    let bytecode = ego::gen_bytecode("print(12); ".to_string());
+    let mut vm = self_vm::new(bytecode);
     vm.run();
 }
--- self/src/lib.rs
+++ self/src/lib.rs
@@ -5,3 +5,7 @@ mod types;
 
 pub mod utils;
 pub mod vm;
+
+pub fn new(bytecode: Vec<u8>) -> vm::Vm {
+    vm::Vm::new(bytecode)
+}
