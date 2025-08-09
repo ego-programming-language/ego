83f8ccd10a568c275d25918aa926befaf6be2d1f
noreplydev <contact.cristiansanchez@gmail.com>
2025-08-09 21:58:09 +0200

    add fix format cargo script

--- ego/src/ast/mod.rs
+++ ego/src/ast/mod.rs
@@ -3,7 +3,7 @@ mod lexer_types;
 mod nodes;
 mod parser;
 
+pub use self::lexer::*;
+pub use self::lexer_types::*;
 pub use self::nodes::*;
 pub use self::parser::*;
-pub use self::lexer_types::*;
-pub use self::lexer::*;
