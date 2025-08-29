a9ad3542872a80a569eb60d3d8af1c9c92319a90
noreplydev <contact.cristiansanchez@gmail.com>
2025-08-29 08:17:58 +0200

    add Engine struct declaration on ai native module

--- self/src/std/ai/mod.rs
+++ self/src/std/ai/mod.rs
@@ -5,8 +5,12 @@ pub mod types;
 
 use crate::{
     memory::MemObject,
+    opcodes::DataType,
     std::ai::members::{do_fn, infer},
-    types::object::func::{Engine, Function},
+    types::object::{
+        func::{Engine, Function},
+        structs::StructDeclaration,
+    },
 };
 
 pub fn generate_struct() -> (String, Vec<(String, MemObject)>) {
@@ -21,8 +25,14 @@ pub fn generate_struct() -> (String, Vec<(String, MemObject)>) {
         vec![], // TODO: load params to native functions
         Engine::Native(do_fn),
     ));
+    let engine_ref = MemObject::StructDeclaration(StructDeclaration {
+        identifier: "Engine".to_string(),
+        fields: vec![("name".to_string(), DataType::Utf8)],
+    });
+
     fields.push(("infer".to_string(), infer_ref));
     fields.push(("do".to_string(), do_ref));
+    fields.push(("Engine".to_string(), engine_ref));
 
     ("ai".to_string(), fields)
 }
